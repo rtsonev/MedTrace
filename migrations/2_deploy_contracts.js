febdbcfd940b8e1567c2ee8d667eeff6f489d9a0
Rostislav Tsonev <rostislav.tsonev@gmail.com>
2018-03-09 17:04:25 +0200

    simple UI

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ var AuthorityOracle = artifacts.require("./AuthorityOracle.sol");
 var MedTrace = artifacts.require("./MedTrace.sol");
 var Medicine = artifacts.require("./Medicine.sol");
 
-module.exports = function(deployer) {
-    deployer.deploy(AuthorityOracle)
-        .then(() => deployer.deploy(MedTrace, AuthorityOracle.address))
+module.exports = function(deployer, network, accounts) {
+    deployer.deploy(AuthorityOracle, {from: accounts[0]})
+        .then(() => deployer.deploy(MedTrace, AuthorityOracle.address, {from:accounts[1]}))
 };
