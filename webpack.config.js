fa8b4238681737d67b76ef550076ae08afb04dbf
Rostislav Tsonev <rostislav.tsonev@gmail.com>
2018-03-10 16:16:52 +0200

    add webpack, some ui, authority tab

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './app/javascripts/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './app/index.html', to: "index.html" },
+      { from: './app/img/meds.png', to: "meds.png" }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+    loaders: [
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  }
+}
