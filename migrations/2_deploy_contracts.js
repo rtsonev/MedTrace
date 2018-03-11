5bf767e36617bca509458b81740c5a69724d8458
Rostislav Tsonev <rostislav.tsonev@gmail.com>
2018-03-11 14:35:50 +0200

    last minute stuff

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-let AuthorityOracle = artifacts.require("./AuthorityOracle.sol");
-let MedTrace = artifacts.require("./MedTrace.sol");
-let Medicine = artifacts.require("./Medicine.sol");
+let AuthorityOracle = artifacts.require('./AuthorityOracle.sol')
+let MedTrace = artifacts.require('./MedTrace.sol')
+let Medicine = artifacts.require('./Medicine.sol')
 
-module.exports = function(deployer, network, accounts) {
-    deployer.deploy(AuthorityOracle, {from: accounts[0]})
-        .then(() => deployer.deploy(MedTrace, AuthorityOracle.address, {from:accounts[1]}))
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(AuthorityOracle, { from: accounts[0] })
+    .then(() => deployer.deploy(MedTrace, AuthorityOracle.address, { from: accounts[1] }))
 };
--- truffle.js
+++ truffle.js
@@ -7,6 +7,16 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*'
+    },
+    ropsten: {
+      network_id: 3,
+      host: 'localhost',
+      port: 8545,
+      gas: 2900000
     }
+  },
+  rpc: {
+    host: 'localhost',
+    post: 8080
   }
 }
